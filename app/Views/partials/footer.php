8b7a01df7797c62f938acc5ea666a5a3bbfff222
mikigatto93 <mikigatto93@gmail.com>
2023-10-03 19:31:21 +0200

    ok megamanu for desktop (todo: make megamenu mobile friendly)

--- app/Views/partials/footer.php
+++ app/Views/partials/footer.php
@@ -110,3 +110,5 @@
     </div>
     <!-- Copyright -->
   </footer>
+
+  
--- app/Views/partials/navbar.php
+++ app/Views/partials/navbar.php
@@ -12,66 +12,122 @@
 
         <li class="nav-item">
 
-          <?php if ($current_page === 'home') :?>
+          <?php if ($current_page === 'home') : ?>
             <a class="nav-link active" aria-current="page" href="#">HOME</a>
-          <?php else: ?>
+          <?php else : ?>
             <a class="nav-link" href="home">HOME</a>
           <?php endif; ?>
         </li>
 
         <li class="nav-item">
-          <?php if ($current_page === 'chi-siamo') :?>
+          <?php if ($current_page === 'chi-siamo') : ?>
             <a class="nav-link active" aria-current="page" href="#">CHI SIAMO</a>
-          <?php else :?>
+          <?php else : ?>
             <a class="nav-link" href="chi-siamo">CHI SIAMO</a>
           <?php endif; ?>
         </li>
 
-        <li class="nav-item dropdown">
-          <?php if ($current_page === 'prodotti') :?>
+        <li class="nav-item dropdown has-megamenu">
+          <?php if ($current_page === 'prodotti') : ?>
             <a class="nav-link dropdown-toggle active" aria-current="page" href="prodotti" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               PRODOTTI
             </a>
-          <?php else: ?>
+          <?php else : ?>
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            PRODOTTI
-          </a>
+              PRODOTTI
+            </a>
           <?php endif; ?>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+
+
+          <div class="dropdown-menu megamenu" role="menu">
+
+            <div class="container-fluid p-0">
+              <div class="d-flex flex-row justify-content-evenly">
+
+                <dl class="megamenu-nav-list">
+                  <dt><a href="finestre-in-PVC-WnD">FINESTRE IN PVC</a></dt>
+                  <dd><a href="#">Konfortline</a></dd>
+                  <dd><a href="#">Square Plus</a></dd>
+                  <dd><a href="#">D+</a></dd>
+                  <dd><a href="#">Bilico</a></dd>
+                  <dd><a href="#">Aluskin</a></dd>
+                </dl>
+
+                <dl class="megamenu-nav-list">
+                  <dt><a href="#">SISTEMI SCORREVOLI <br> IN PVC</a></dt>
+                  <dd><a href="#">Smart Slide</a></dd>
+                  <dd><a href="#">HST</a></dd>
+                  <dd><a href="#">Slide Plus</a></dd>
+                  <dd><a href="#">PSK</a></dd>
+                </dl>
+
+                <dl class="megamenu-nav-list">
+                  <dt><a href="#">FINESTRE IN LEGNO</a></dt>
+                  <dd><a href="#">Profilo 1</a></dd>
+                  <dd><a href="#">Profilo 2</a></dd>
+                  <dd><a href="#">Profilo 3</a></dd>
+                </dl>
+
+
+                <div>
+                  <dl class="megamenu-nav-list">
+                    <dt><a href="#">SISTEMI SCORREVOLI <br> IN LEGNO </a></dt>
+                  </dl>
+
+                  <dl class="megamenu-nav-list">
+                    <dt><a href="#">PORTE IN LEGNO</a></dt>
+                  </dl>
+
+                  <dl class="megamenu-nav-list">
+                    <dt><a href="#">BLINDATI</a></dt>
+                  </dl>
+
+                </div>
+                <dl class="megamenu-nav-list">
+                  <dt><a href="#">SISTEMI OSCURANTI</a></dt>
+                  <dd><a href="#">Scuri</a></dd>
+                  <dd><a href="#">Persiane</a></dd>
+                  <dd><a href="#">Zanzariere</a></dd>
+                </dl>
+
+              </div>
+
+            </div>
+
+          </div> <!-- dropdown-mega-menu.// -->
+
         </li>
-        
+
         <li class="nav-item">
-          <?php if ($current_page === 'manutenzione') :?>
+          <?php if ($current_page === 'manutenzione') : ?>
             <a class="nav-link active" aria-current="page" href="#">MANUTENZIONE</a>
-          <?php else :?>
+          <?php else : ?>
             <a class="nav-link" href="manutenzione">MANUTENZIONE</a>
           <?php endif; ?>
         </li>
 
         <li class="nav-item">
-          <?php if ($current_page === 'galleria') :?>
-            <a class="nav-link active" aria-current="page" href="#">GALLERIA</a>
-          <?php else :?>
-            <a class="nav-link" href="galleria">GALLERIA</a>
+          <?php if ($current_page === 'lavori') : ?>
+            <a class="nav-link active" aria-current="page" href="#">LAVORI</a>
+          <?php else : ?>
+            <a class="nav-link" href="lavori">LAVORI</a>
           <?php endif; ?>
         </li>
-
+          
+        <!--
         <li class="nav-item">
-          <?php if ($current_page === 'showroom') :?>
+          <?php if ($current_page === 'showroom') : ?>
             <a class="nav-link active" aria-current="page" href="#">SHOWROOM</a>
-          <?php else :?>
+          <?php else : ?>
             <a class="nav-link" href="showroom">SHOWROOM</a>
           <?php endif; ?>
         </li>
+          -->
 
         <li class="nav-item">
-          <?php if ($current_page === 'contatti') :?>
-          <a class="nav-link active" aria-current="page" href="#">CONTATTI</a>
-          <?php else :?>
+          <?php if ($current_page === 'contatti') : ?>
+            <a class="nav-link active" aria-current="page" href="#">CONTATTI</a>
+          <?php else : ?>
             <a class="nav-link" href="contatti">CONTATTI</a>
           <?php endif; ?>
         </li>
